0f9ef05d99ac5b33e01ed84aac7650d713617896
Maria <maria.altyeva@gmail.com>
2019-07-09 15:31:16 +0800

    seeds

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,4 @@
 class Cocktail < ApplicationRecord
+  acts_as_taggable_on :categories
+  acts_as_taggable_on :flavors
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@ url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.j
 
 Cocktail.delete_all if Rails.env.development?
 
+
+flavors = ['sweet', 'citrusy', 'sour', 'strong', 'bitter']
+
 cocktails = JSON.parse(open(url).read)
 
 cocktails.each do |cocktail|
-  Cocktail.create!(name: cocktail["name"], glass: cocktail["glass"], preparation: cocktail["preparation"])
+  Cocktail.create!(name: cocktail["name"],
+                   glass: cocktail["glass"],
+                   preparation: cocktail["preparation"],
+                   category_list: cocktail["category"],
+                   flavor_list: flavors.sample)
 end
