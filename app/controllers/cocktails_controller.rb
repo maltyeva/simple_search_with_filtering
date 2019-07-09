e84f4a23335a9bb74e64d5276c02fd250dde779e
Maria <maria.altyeva@gmail.com>
2019-07-09 22:00:49 +0800

    added form and search to backend

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,10 @@
 class CocktailsController < ApplicationController
   def index
-    @cocktails = Cocktail.all
-    @search = params["search"]
-
-    if @search.present?
-      @name = @search["name"]
-      @cocktails = Cocktail.where("name ILIKE ?", "%#{@name}%")
+    if params["search"]
+      @filter = params["search"]["flavors"].concat(params["search"]["strengths"]).flatten.reject(&:blank?)
+      @cocktails = @filter.empty? ? Cocktail.all : Cocktail.all.tagged_with(@filter, any: true)
+    else
+      @cocktails = Cocktail.all
     end
   end
 end
